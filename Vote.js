bbbcc075153407bfdc97d7b51e3d27d55a92c5fa
Alex Levin <62648515+AlexLevin-LA-NY@users.noreply.github.com>
2020-09-09 07:01:38 -0400

    Add files via upload

---
+++ Vote.js
@@ -0,0 +1,23 @@
+function func1() {
+    var chbox;
+    chbox = document.getElementById("one");
+
+    if (chbox.checked) {
+        alert("Are you sure you wanna vote for the best President");
+        alert("Congratulation, You've just voted for thew best President");
+    } else {
+        alert("We are country of freedom, hence feel free to change your mind anytime.");
+    }
+}
+
+function func2() {
+    var chbox;
+    chbox = document.getElementById("two");
+
+    if (chbox.checked) {
+        alert("What do you have to lose by trying something new like a Trump?");
+        alert("Congratulation, You've just made your choice");
+    } else {
+        alert("Okay, I see. You don't want to make America great condom");
+    }
+}
